884e0693abb7b336289e80d380606c5a1b0f2ecd
Andrew Rabert <ar@nullsum.net>
2021-03-27 17:31:14 -0400

    Add opus test

--- tests/test_app.py
+++ tests/test_app.py
@@ -40,3 +40,27 @@ class TestApp(unittest.TestCase):
         # TODO test output
         # self.assertEqual(proc.stderr, b'')
         # self.assertEqual(proc.stdout, b'')
+
+    def test_transcodes_flac_to_opus(self):
+        source_dir = TMP / 'source'
+        source_dir.mkdir()
+        target_dir = TMP / 'target'
+        helpers.ffmpeg.generate_silence(1, source_dir / 'audio.flac')
+
+        subprocess.run(
+            ['harmonize', '--codec', 'opus', str(source_dir), str(target_dir)],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            check=True)
+
+        metadata = helpers.ffprobe.get_metadata(target_dir / 'audio.opus')
+
+        self.assertEqual('ogg', metadata['format']['format_name'])
+        self.assertEqual(1, len(metadata['streams']))
+        self.assertEqual('opus', metadata['streams'][0]['codec_name'])
+        # mp3 will not be exact duration as input
+        self.assertTrue(1 <= float(metadata['format']['duration']) <= 1.1)
+
+        # TODO test output
+        # self.assertEqual(proc.stderr, b'')
+        # self.assertEqual(proc.stdout, b'')
